fcf0dc74eed5d5c5b683e6b09d7d07e9a377bf82
Harry Liu <2411mail@gmail.com>
2020-04-15 09:54:15 +0800

    add config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CellsModule } from './cells/cells.module';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [CellsModule],
+  imports: [
+    CellsModule,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
