129e67a38d87ba839f8057775f35ad9a6e7d37e7
Harry Liu <2411mail@gmail.com>
2020-04-16 14:19:55 +0800

    add swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configService } from './config/config.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (!configService.isProduction()) {
+    const options = new DocumentBuilder()
+      .setTitle('Synapse Server APIs')
+      .setDescription('Feel free to try out Synapse Server APIs')
+      .setVersion('0.0.1')
+      // .addTag('Synapse')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
