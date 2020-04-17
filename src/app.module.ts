cc07bfa71d6ab60cf8b0f9024680d7fe5145e64f
Harry Liu <2411mail@gmail.com>
2020-04-17 10:47:47 +0800

    add block module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
 import { CellModule } from './cell/cell.module';
+import { BlockModule } from './block/block.module';
 
 @Module({
   imports: [
     CellModule,
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    BlockModule
   ],
   controllers: [AppController],
   providers: [AppService],
