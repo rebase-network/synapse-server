8a250f8e325f492d388690767d4efc4c280e3659
Harry Liu <2411mail@gmail.com>
2020-04-18 21:51:15 +0800

    able to update block num automatically into db

--- src/block/block.module.ts
+++ src/block/block.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlockController } from './block.controller';
 import { BlockService } from './block.service';
 import { Block } from '../model/block.entity';
+import { SyncStat } from '../model/syncstat.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Block])],
+  imports: [TypeOrmModule.forFeature([Block, SyncStat])],
   controllers: [BlockController],
   providers: [BlockService]
 })
--- src/block/block.service.ts
+++ src/block/block.service.ts
@@ -4,22 +4,44 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Block } from '../model/block.entity';
 import { CkbService } from '../ckb/ckb.service';
-
+import { SyncStat } from '../model/syncstat.entity';
 
 @Injectable()
 export class BlockService extends NestSchedule {
   constructor(
-    @InjectRepository(Block) private readonly repo: Repository<Block>,
-    private readonly ckbService: CkbService,
+    @InjectRepository(Block) private readonly blockRepo: Repository<Block>,
+    @InjectRepository(SyncStat) private readonly syncStatRepo: Repository<SyncStat>,
+    private readonly ckbService: CkbService
   ) {
     super();
   }
 
   private readonly ckb = this.ckbService.getCKB();
 
+  /**
+   * sync blocks from blockchain
+   */
   @Interval(5000)
   async sync() {
     const header = await this.ckb.rpc.getTipHeader();
     console.log(new Date(Date.now()), 'header: ', header)
+    const currentTip = parseInt(header.number, 16);
+    await this.updateTip(currentTip);
+  }
+
+  /**
+   * update last syncing block
+   * @param tip block number
+   */
+  async updateTip(tip: number): Promise<any> {
+    let block = await this.syncStatRepo.findOne();
+    console.log('block: ', block)
+    if (block) {
+      block.tip = tip;
+    } else {
+      block = this.syncStatRepo.create({ tip })
+    }
+    this.syncStatRepo.save(block)
+    return { block };
   }
 }
