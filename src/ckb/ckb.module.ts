377a9085ff41998e6489394d42e8c7bc07396144
Harry Liu <2411mail@gmail.com>
2020-04-18 19:11:24 +0800

    do not need to inject the dependency due to configService is a instance

--- src/ckb/ckb.module.ts
+++ src/ckb/ckb.module.ts
@@ -1,10 +1,8 @@
 import { Module, Global } from '@nestjs/common';
 import { CkbService } from './ckb.service';
-// import { ConfigModule } from '../config/config.module';
 
 @Global()
 @Module({
-  // imports: [ConfigModule],
   providers: [CkbService],
   exports: [CkbService],
 })
