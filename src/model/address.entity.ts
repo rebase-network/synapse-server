9f15a863f7bf611845c863044bbd4e3e5badb0fd
liushooter <shooter.hero@gmail.com>
2020-05-06 16:47:47 +0800

    add address entity

---
+++ src/model/address.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'address' })
+export class Address {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column('varchar')
+  pubKeyHash: string;
+
+  @Column('varchar')
+  lockHash: string;
+
+  @Column('varchar')
+  address: string;
+
+  @Column('bigint', { default: 0 })
+  balance: number;
+
+  @Column('bigint', { default: 0 })
+  liveCellCount: number;
+
+  @Column('bigint', { default: 0 })
+  txCount: number;
+}
