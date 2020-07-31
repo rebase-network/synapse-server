6c21f8bd0e5298d2d7676f8af0efceab5cfdf3e1
syuukawa <syuukawa@hotmail.com>
2020-07-31 17:27:02 +0800

    mod condition  for get livecell

--- src/cell/cell.repository.ts
+++ src/cell/cell.repository.ts
@@ -9,18 +9,12 @@ export class CellRepository extends Repository<Cell> {
       .andWhere('cell.capacity >:sendCapactity', {
         sendCapactity: capacity,
       })
-      .andWhere('cell.outputData =  :outputData', {
-        outputData: '0x',
-      })
       .getOne();
   }
 
   public async queryByQueryObjAndStepPage(queryObj, step, page) {
     return await this.createQueryBuilder('cell')
-      .where(queryObj)
-      .andWhere('cell.outputData =  :outputData', {
-        outputData: '0x',
-      })      
+      .where(queryObj) 
       .orderBy('cell.capacity', 'DESC')
       .limit(step)
       .offset(step * page);
